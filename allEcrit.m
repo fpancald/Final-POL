c1b2e3bb9e6b4926d5c7362616daa32133db10fb
fpancald <fpancald@nd.edu>
2014-12-29 12:53:39 -0500

    add debugged version of ecrit.m->ecrit2.m and started ranker function ranker.m

--- allEcrit.m
+++ allEcrit.m
@@ -3,7 +3,8 @@ format long
 n=length(A);
 E=[];
 for i=2:n
-    E=[E;ecrit(A,i,c,p)];
+%     E=[E;ecrit(A,i,c,p)];
+    E=[E;ecrit2(A,i,c,p)];
 end
 E=flipud(sort(unique(E)));
 end
---
+++ ranker.m
@@ -0,0 +1,8 @@
+l=length(A(1,1,:));
+c=1;
+p=0.9;
+for i=1:l
+    r=allEcrit(A(:,:,i),c,p);
+    i
+    r
+end
