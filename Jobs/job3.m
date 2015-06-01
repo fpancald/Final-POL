1598bb02277773460b70bb7bf2906dc5610c1742
fpancald <fpancald@nd.edu>
2015-06-01 17:40:59 -0400

    mod job3 finish

--- Jobs/job3.m
+++ Jobs/job3.m
@@ -1,7 +1,15 @@
+% cd ..
+% scriptjob3
+% cd rand_size_5_15_total_550_50_each
+% save('matlab.mat','-v7.3')
+% cd ..
+% scriptbysize
+% cd rand_size_5_15_total_550_50_each
+% save('matlab2.mat','HH','-v7.3')
+
 cd ..
-scriptjob3
 cd rand_size_5_15_total_550_50_each
-save('matlab.mat','-v7.3')
+load('matlab.mat')
 cd ..
 scriptbysize
 cd rand_size_5_15_total_550_50_each
