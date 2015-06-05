5506c67f9cfb7e61ab9762884c657c8fcab5cda6
fpancald <fpancald@nd.edu>
2015-06-05 06:20:00 -0400

    minor changes to job5 script

--- scriptjob5.m
+++ scriptjob5.m
@@ -1,12 +1,14 @@
 %mean and std in window as energy maximized with respect to mean energy
-% entxwps=zeros(nME,m-1);
-% entstdwps=zeros(nME,m-1);
-% for j=2:m
-%     [exwp,estdwp]=energymeanstd(10.^(mx/nME:mx/nME:mx),HH{7}(:,j-1),10.^(Es));
-%     entxwps(:,j-1)=exwp;
-%     entstdwps(:,j-1)=estdwp;
-% end
-number=4;
+entxwps=zeros(nME,m-1);
+entstdwps=zeros(nME,m-1);
+in=10;
+for j=2:m
+    [exwp,estdwp]=energymeanstd(10.^(mx/nME:mx/nME:mx),HH{in}(:,j-1),10.^(Es));
+    entxwps(:,j-1)=exwp;
+    entstdwps(:,j-1)=estdwp;
+end
+max(max(HH{in}))
+number=max(max(HH{in}))
 figure(1)
 plot(p(2:end),entxwps(1:nME,:)/number);
 K=K+1;
