19991eb92d4fa5e5de45da3b1e38cf9488d5b67d
fpancald <fpancald@nd.edu>
2015-06-10 04:31:25 -0400

    new plots + changes to job7

--- plottermeanstd.m
+++ plottermeanstd.m
@@ -7,7 +7,11 @@ for in=n1:n2
     standdev(:,in)=entstdwps{in}(numener,:)/number;
 end
 set(0,'DefaultAxesLineStyleOrder',{'-','--',':'})
-% plot(p(2:end),mean(:,n1:n2));
-% plot(p(2:end),standdev(:,n1:n2));
+figure(1)
+plot(p(2:end),mean(:,n1:n2));
+figure(2)
+plot(p(2:end),standdev(:,n1:n2));
+figure(3)
 plot(n1:n2,mean(1:100:1001,n1:n2)');
-% plot(n1:n2,standdev(1:100:1001,n1:n2)');
+figure(4)
+plot(n1:n2,standdev(1:100:1001,n1:n2)');
---
+++ scriptbysize2.m
@@ -0,0 +1,14 @@
+for ns=2:n4
+    HH{ns}=zeros(mm-1,m-1); 
+end
+for i=2:mm
+    for j=2:m
+        for h=1:l
+            s=length(T{h});
+%             if Es(i)>LE(h,j)
+            if Es(i)>LE(h,j)-log10(s)%gives energy per cell
+                HH{s}(i-1,j-1)=HH{s}(i-1,j-1)+1;
+            end
+        end
+    end
+end
