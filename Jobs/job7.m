9971b432a555b7645d9e4af9857ab1e213c940cb
fpancald <fpancald@nd.edu>
2015-06-10 04:35:39 -0400

    correct job7

--- Jobs/job7.m
+++ Jobs/job7.m
@@ -3,6 +3,14 @@
 % mkdir rand_size_11_15_and_31_35_total_500_50_each
 % cd rand_size_11_15_and_31_35_total_500_50_each
 % save('matlab.mat','-v7.3')
+% cd ..
+% scriptbysize2
+% cd rand_size_11_15_and_31_35_total_500_50_each
+% save('matlab2.mat','HH','-v7.3')
+
+cd ..
+cd rand_size_11_15_and_31_35_total_500_50_each
+load('matlab.mat')
 cd ..
 scriptbysize2
 cd rand_size_11_15_and_31_35_total_500_50_each
